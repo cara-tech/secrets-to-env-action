ab40dafc23940f966edd4b858d71d22f067269c6
Marius Seskauskas <marius.seskauskas@cara-online.com>
2023-04-25 14:12:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ const inputs = input_keys.map((k) => [k, core.getInput(k)]);
 const files = main(Object.fromEntries(inputs));
 const file_names = files.map(([file_name]) => file_name);
 
+try {
+  files.map(([file_name, env_string]) =>
+    fs.writeFileSync(file_name, "test string")
+  );
+  console.log(makeSuccessMessage(file_names));
+} catch (err) {
+  console.error(err);
+}
+
+/*
 Promise.all(
   files.map(([file_name, env_string]) => fs.writeFile(file_name, "test string"))
 )
   .then(() => makeSuccessMessage(file_names))
   .then(console.log)
   .catch((err) => core.setFailed(err.message));
+*/
